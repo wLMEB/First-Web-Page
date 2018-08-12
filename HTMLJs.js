8969c1433533897baa792064c778633c02797544
wLMEB <42008573+wLMEB@users.noreply.github.com>
2018-08-12 19:27:16 -0400

    upload

---
+++ HTMLJs.js
@@ -0,0 +1,27 @@
+//window.alert("");
+function displayMatch() {
+  var x = document.getElementById("match");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+}
+
+function displayserve() {
+  var x = document.getElementById("serve");
+if (x.style.display === "none") {
+  x.style.display = "block";
+} else {
+  x.style.display = "none";
+}
+}
+
+function displaycourt() {
+  var x = document.getElementById("court");
+  if (x.style.display === "none") {
+      x.style.display = "block";
+  } else {
+      x.style.display = "none";
+  }}
